79e85a9d695fc262c571a0fe59302c289fa484bb
Manh <nguyenducmanhh201@gmail.com>
2023-02-21 21:22:15 +0700

    first commit

---
+++ src/helper/i18n.ts
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import vi from 'locales/vi.json';
+import en from 'locales/en.json';
+import ja from 'locales/ja.json';
+
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: 'ja',
+    debug: false,
+    lng: 'ja',
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      vi: { translation: vi },
+      en: { translation: en },
+      ja: { translation: ja },
+    },
+  });
