6bc8a79a25feeddd332262753e90b848a592c9c4
tvdat <vandat.bk52@gmail.com>
2023-04-04 18:56:43 +0700

    add card components

--- src/modules/home/page/home-page.tsx
+++ src/modules/home/page/home-page.tsx
@@ -1,7 +1,17 @@
+import { Box, Grid, Stack, calc } from '@chakra-ui/react'
 import React from 'react'
+import { CardItem } from '~/components'
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Grid templateColumns={{lg: 'repeat(3, 1fr)', xl: 'repeat(4, 1fr)', '2xl': 'repeat(5, 1fr)' }} gap={6}>
+      {Array(11)
+        .fill(0)
+        .map((v, i) => (
+          <Box key={i} w={'100%'}>
+            <CardItem />
+          </Box>
+        ))}
+    </Grid>
   )
 }
