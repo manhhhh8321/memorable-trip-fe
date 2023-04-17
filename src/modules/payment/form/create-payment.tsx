ed540a6f6dd71eab1a0d42d6db727f9577c4c603
tvdat <vandat.bk52@gmail.com>
2023-04-17 18:18:45 +0700

    ui payment

--- src/modules/payment/form/create-payment.tsx
+++ src/modules/payment/form/create-payment.tsx
@@ -1,12 +1,14 @@
 import { Box, HStack, VStack } from '@chakra-ui/react'
 import React from 'react'
 import { LeftPayment } from './payment-left'
+import { RightPayment } from './payment-right'
 
 export const CreatePayment = () => {
   return (
-    <Box>
-      <HStack>
+    <Box w={'100%'} pt={10} minH={'900px'}>
+      <HStack w={'100%'} pos='relative' spacing={'80px'} alignItems={'start'} justifyContent='center'>
         <LeftPayment />
+        <RightPayment />
       </HStack>
     </Box>
   )
